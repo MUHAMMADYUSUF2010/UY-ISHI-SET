9686340914aefc02ea2d7cebee2443f220b0d92f
MUHAMMADYUSUF2010 <100182079+MUHAMMADYUSUF2010@users.noreply.github.com>
2022-02-22 09:47:28 +0500

    Add files via upload

---
+++ UY ISHI.py
@@ -0,0 +1,47 @@
+#1
+sonlar1=(10, 20, 30, 40, 50)
+sonlar2=sonlar1[::-1]
+print(sonlar2)
+
+
+#2
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+
+# tuple1[0] = 'Orange'
+# tuple1[1] = [10, 20, 30]
+# list1[1][1] = 20
+ 
+print(tuple1[1][1])
+
+
+#3
+qiymat1 = (11, 22)
+qiymat2 = (99, 88)
+qiymat1, qiymat2 = qiymat2, qiymat1
+print(qiymat1)
+print(qiymat2)
+
+
+#4
+natija1 = (11, 22, 33, 44, 55, 66)
+natija2 = natija1[3:-1]
+print(natija2)
+
+
+#5
+tuple1 = (11, [22, 33], 44, 55)
+tuple1[1][0] = 222
+print(tuple1)
+
+
+#6
+mashinalar=("Lamborghini", "Bugatti", "Chevrolet")
+print(mashinalar[-1] )
+
+  
+#7
+ichimliklar=("Fanta", "Choy", "Tropic", "Flash")
+
+
+#8
